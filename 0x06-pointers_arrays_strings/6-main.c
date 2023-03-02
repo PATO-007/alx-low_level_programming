bd36d9a970607c8ca010b2fa096776903d5c96ed
PATO007 <patrickgodwin21@gmail.com>
2023-03-02 10:55:41 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes. \nhello world! hello-world 0123456hello world\t
-		hello world.hello world\n"
-		char *ptr;
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello world 0123456hello world\thello world.hello world\n";
 
-	ptr = cap_string(str);
-	printf("%s", ptr);
-	printf("%s", str);
-	return (0);
+char *ptr;
+
+ptr = cap_string(str);
+printf("%s", ptr);
+printf("%s", str);
+return (0);
 }
